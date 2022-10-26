2d09a374e14bdc94e3f1cbc6ee85bd511177e339
nazmulhasan8 <nazmulhasancse7@gmail.com>
2022-10-27 01:25:04 +0600

    Assignment-Hero-Tenth-Server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+// courseCategories and courseDetails
+const courseCategories = require('./data/courseCategories.json');
+const courseDetails = require('./data/courseDetails.json');
+
+app.get('/courseCategories', (req, res) => {
+    res.send(courseCategories)
+});
+
+app.get('/courseDetails', (req, res) =>{
+    res.send(courseDetails);
+});
+
+app.get('/courseDetails/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourseDetails = courseDetails.find(n => n._id === id);
+    res.send(selectedCourseDetails);
+});
+
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+app.listen(port, () => {
+    console.log('Dragon News Server running on port', port);
+})
+//explore the Express api
+module.exports = app;
